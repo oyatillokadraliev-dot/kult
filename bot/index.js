0354e92599e44cf41f01435552204f8f58c23e66
oyatillokadraliev-dot <oyatillokadraliev@gmail.com>
2026-09-18 19:12:25 +0400

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -23,7 +23,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
-privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+privateKey: process.env.FIREBASE_PRIVATE_KEY ? Buffer.from(process.env.FIREBASE_PRIVATE_KEY, 'base64').toString('utf8') : undefined,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     clientId: process.env.FIREBASE_CLIENT_ID,
   }),
